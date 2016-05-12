f6b00f48412c5c7827b0f94e80bbff5844e432b4
tapti <taptione@gmail.com>
2016-05-13 01:05:57 +0300

    add & modify students_list.html(161 str. of book)

--- stud/views.py
+++ stud/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def students_list(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'students/students_list.html', {})
