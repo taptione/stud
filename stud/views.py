da825ce00d23b9a6061642f34502b9c378f2d5f1
tapti <taptione@gmail.com>
2016-05-15 07:16:01 +0300

    add 2 view all students, change student_list.html, use circle tag (184 str. of book)

--- stud/views.py
+++ stud/views.py
@@ -6,7 +6,24 @@ from django.http import HttpResponse
 
 #views 4 stud
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Ігор',
+         'last_name': u'Горобець',
+         'ticket': 700,
+         'image': 'img/tweat.jpg'},
+        {'id': 2,
+         'first_name': u'Ігор',
+         'last_name': u'Семенюк',
+         'ticket': 777,
+         'image': 'img/sem.png'},
+        {'id': 3,
+         'first_name': u'Совпадєніє?',
+         'last_name': u'Не думаю',
+         'ticket': 707,
+         'image': 'img/kfc.jpg'},
+    )
+    return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
