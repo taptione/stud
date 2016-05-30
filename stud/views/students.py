36b6b02e077e14f2d55cd259c415bc7bcb1347b6
tapti <taptione@gmail.com>
2016-05-31 00:53:11 +0300

    use paginator with order(256 str. of book)

--- stud/views/students.py
+++ stud/views/students.py
@@ -3,6 +3,7 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from ..models import Student
 
@@ -16,6 +17,18 @@ def students_list(request):
         students = students.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
             students = students.reverse()
+
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page
+        students = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range, deliver last page of results
+        students = paginator.page(paginator.num_pages)
 
     return render(request, 'students/students_list.html', {'students': students})
 
